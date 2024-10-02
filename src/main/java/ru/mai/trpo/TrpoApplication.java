f14f2123aea5b9b9af4d317e27082c29c51ec831
Чумаченко Артем Александрович (4104080) <chumachenko@dev.vtb.ru>
2024-10-02 23:31:40 +0300

    init

---
+++ src/main/java/ru/mai/trpo/TrpoApplication.java
@@ -0,0 +1,13 @@
+package ru.mai.trpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mai/trpo/TrpoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mai.trpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
